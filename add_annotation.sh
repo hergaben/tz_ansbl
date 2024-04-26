cd627dae8af537c0f22d97b59087edd681c80e73
tohir <toxir.aliev.96@mail.ru>
2024-04-26 17:09:52 +0300

    v0.1 Finally

---
+++ add_annotation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+GRAFANA_URL="http://192.168.2.136:3000/api/annotations"
+API_KEY="eyJrIjoiMWdUaGFYcmhjeW5iQ0o2QnA5UkpmYk0yTjMzT2xiTEsiLCJuIjoidG9oaXJfYW5ub3RhdGlvbl9rZXlfMTIzIiwiaWQiOjF9"
+
+TIME=$(date +%s)000
+
+DATA_JSON=$(cat <<EOF
+{
+  "dashboardId": 2,
+  "time": $TIME,
+  "tags": ["ansible", "deployment"],
+  "text": "Ansible playbook deployment started"
+}
+EOF
+)
+
+curl -X POST "$GRAFANA_URL" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $API_KEY" \
+    -d "$DATA_JSON"
+
